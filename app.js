3952c2d7acaebdd8950d456e1669666ea5bf95b5
Katouz <tn.thanapat_st@tni.ac.th>
2022-07-16 19:45:40 +0700

    Change to linear-gradient

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const color2 = document.querySelector(".color2");
 generateHex();
-btn.addEventListener("click", () => {
-    generateHex();
-});
+btn.addEventListener("click", generateHex);
 function generateHex(){
     randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#"+randomColor;
+    randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.background = `linear-gradient(to right, #${randomColor}, #${randomColor2})`;
     color.textContent = "#"+randomColor;
+    color2.textContent = "#"+randomColor2;
 }
